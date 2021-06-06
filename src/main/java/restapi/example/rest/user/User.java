60aa5e66e92a59c93cd566883e6a0208763e4f1f
unknown <jorammanoah1@gmail.com>
2021-06-05 23:16:12 -0700

    Added user class

---
+++ src/main/java/restapi/example/rest/user/User.java
@@ -0,0 +1,11 @@
+package restapi.example.rest.user;
+
+public class User {
+    private  int id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String password;
+    private String token;
+    private Boolean isAuthenticated;
+}
