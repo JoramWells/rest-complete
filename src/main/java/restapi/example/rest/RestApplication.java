385d9963a48eae6ec2c865d7bc6b74ee3eb8befd
unknown <jorammanoah1@gmail.com>
2021-06-05 23:24:05 -0700

    Added user index routes

--- src/main/java/restapi/example/rest/RestApplication.java
+++ src/main/java/restapi/example/rest/RestApplication.java
@@ -2,12 +2,25 @@ package restapi.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import restapi.example.rest.user.User;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class RestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
+	}
+
+	@GetMapping("/user")
+	public List<User> hello(){
+		return List.of(
+				new User(1,"joram","bramuel","jorammanoah1@gmail","JoramWells18.","sjchkxhcjxhc",Boolean.TRUE)
+		);
 	}
 
 }
